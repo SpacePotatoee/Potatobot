f23434c21f1e0d439de74c618b9425c4828ea4e4
Raine <101475530+chromenberg@users.noreply.github.com>
2025-09-11 12:19:22 +0100

    bracket

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ client.on(Events.MessageCreate, async (message) => {
 })
 
 client.on("messageCreate", async (msg) => {
-    if (msg.author.bot || msg.channel.id === config.honeypot_channel) return
+    if (msg.author.bot || (msg.channel.id === config.honeypot_channel)) return
     
     if ((msg.content.includes("<@1360807782001148134>")||msg.content.toLowerCase().includes("potatobot")) && (msg.content.toLowerCase().includes("sucks")||msg.content.toLowerCase().includes("i hate")||msg.content.toLowerCase().includes("is bad")||msg.content.toLowerCase().includes("is ass")||msg.content.toLowerCase().includes("beat our child")||msg.content.toLowerCase().includes("disown"))) {
         msg.react("😢")
@@ -154,4 +154,5 @@ client.on(Events.ClientReady, async () => {
 })
 
 module.exports.client = client
+
 
